ea8587ddc382c97b2755e3790449a9e556156413
Nicodemus Kiplangat <nicodemusk84@gmail.com>
2024-06-28 10:44:07 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance) {
+    if (distance <= 400) {
+      return 'This one is on me!';
+    } else if (distance > 400 && distance <= 2000) {
+      return 'That will be twenty bucks.';
+    } else if (distance > 2000 && distance <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+    } else {
+      return 'No can do.';
+    }
+  }
+  
+  
+    // Write your code here!
+  
+  
+  function ternaryCheckCity(checkCity){
+    if (checkCity==='NYC'){
+    return "Ok, sounds good."
+  }
+  else if(checkCity!='NYC'){
+    return "No go."
+  
+  }
+    // Write your code here!
+  }
+  
+  function switchOnCharmFromTip(tip){
+    switch (tip) {
+      case "generous":
+        return 'Thank you so much.';
+      case "not as generous":
+        return 'Thank you.';
+      default:
+        return 'Bye.'
+    }
+    // Write your code here!
+  }
